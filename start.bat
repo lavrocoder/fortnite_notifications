129b608659503992ef0463f83b7802936c740c33
Evgeny Lavrov <evgeny.lavrov2002@mail.ru>
2025-10-29 19:16:47 +0700

    version 0.0.4

--- start.bat
+++ start.bat
@@ -1,26 +1,23 @@
 @echo off
 
-REM Проверка, установлена ли утилита uv
 where uv >nul 2>nul
 if %errorlevel% neq 0 (
-    echo Утилита uv не найдена. Устанавливаю...
+    echo Installing UV...
     powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
     if %errorlevel% neq 0 (
-        echo Ошибка установки uv. Проверьте установку Python и pip.
+        echo Installing error
         exit /b 1
     )
-) else (
-    echo Утилита uv уже установлена.
 )
 
 echo.
-echo Выполняю uv sync...
+echo uv sync...
 uv sync --link-mode=copy
 if %errorlevel% neq 0 (
-    echo Ошибка при выполнении uv sync.
+    echo uv sync error.
     exit /b 1
 )
 
 echo.
-echo Запускаю uv run main.py...
+echo uv run main.py...
 uv run main.py
